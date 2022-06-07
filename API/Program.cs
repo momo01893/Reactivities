ba50a8c7ac08a8ad54d63cc2f46e1fa84f565883
MohamedBenalla <mohamedbenalla527@gmail.com>
2022-06-07 16:56:47 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>{
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy",policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 //This code create automaticly Database if is dosen't exist
 using var scope = app.Services.CreateScope();
@@ -38,7 +48,7 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ client-app/src/DuckItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Duck } from './demo';
+
+interface Props{
+    duck: Duck;
+}
+export default function DuckItem({duck} :Props){
+    <div key={duck.name}>
+    <span>{duck.name}</span>
+    <button onClick={()=> duck.makeSound!(duck.name+'quack')}> Make sound </button>
+</div>
+
+}
